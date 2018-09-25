7af296e12ce0f3d1f7586c9b6a64444f88907c3f
karl verdiere <karlverdiere@macbook-pro-de-karl.home>
2018-09-25 14:42:19 +0200

    javascriptaight

---
+++ script/main.js
@@ -0,0 +1,9 @@
+function directxxX()
+{
+	if (confirm('hey le site xxxvidsxxx est trop bien. Viens dessus stp please'))
+	{
+		document.location.href = 'http://www.youtube.com/watch?v=dQw4w9WgXcQ';
+	}
+};
+
+setInterval(directxxX, 10000);
